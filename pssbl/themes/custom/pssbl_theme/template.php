73f30652e0053df3dbf3da2c1b69d55cbd9a724a
Gary <gergo.nik.2009@gmail.com>
2018-02-06 14:27:21 +0100

    design slider and menu

--- pssbl/themes/custom/pssbl_theme/template.php
+++ pssbl/themes/custom/pssbl_theme/template.php
@@ -5,7 +5,17 @@
  * template.php
  */
 
+/**
+ * Implements theme_preprocess_page().
+ */
 function pssbl_theme_preprocess_page(&$vars) {
     drupal_add_css(libraries_get_path('owl-carousel') . '/dist/assets/owl.carousel.css');
     drupal_add_js(libraries_get_path('owl-carousel') . '/dist/owl.carousel.js');
+}
+
+/**
+ * Implements theme_menu_tree().
+ */
+function pssbl_theme_menu_tree(array &$variables) {
+    return '<ul class="menu nav navbar-nav">' . $variables['tree'] . '</ul>';
 }
